b698f12d2fadedf26d4f33cf2f5729d535d4e84f
Russx <72259829+Russx@users.noreply.github.com>
2020-11-30 12:50:29 +0000

    Update WordCount.java

--- WordCount.java
+++ WordCount.java
@@ -21,11 +21,11 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
-public class WordCount extends Configured implements Tool {
+public class WordCount extends Configured  {
 
     public static String inputPath = "";
     public static String outputPath = "";
-    public static class AnagramSortedValuesMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text>
+    public static class AnagramMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text>
     {
 
         // Input Key: Line Number
@@ -45,10 +45,10 @@ public class WordCount extends Configured implements Tool {
         }
     }
 
-    public static class AnagramSortedValuesOutputValueGroupingComparator extends WritableComparator
+    public static class AnagramOutputValueGroupingComparator extends WritableComparator
     {
 
-        public AnagramSortedValuesOutputValueGroupingComparator()
+        public AnagramOutputValueGroupingComparator()
         {
             super(Text.class, true);
         }
@@ -68,7 +68,7 @@ public class WordCount extends Configured implements Tool {
             return naturalKey1.compareTo(naturalKey2);
         }
     }
-    public static class AnagramSortedValuesReducer extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+    public static class AnagramReducer extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
 
         ArrayList<String> listOfLists = new ArrayList<>();
         ArrayList<String> wordCount = new ArrayList<>();
@@ -110,7 +110,6 @@ public class WordCount extends Configured implements Tool {
                         Arrays.stream(newValue.split(", "))
                                 .distinct().toArray(String[]::new)).map(String::new).collect(Collectors.joining(", "));
                 listOfLists.add(newValue.split(", ").length + ", " + newValue);
-                output.collect(new Text("b"), new Text("b"));
             }
         }
 
@@ -118,7 +117,7 @@ public class WordCount extends Configured implements Tool {
         public void close() {
             Collections.sort(listOfLists);
 
-            String filePath = "output/final.txt";
+            String filePath = outputPath+"/final.txt";
             try {
                 File f1 = new File(filePath);
 
@@ -141,102 +140,105 @@ public class WordCount extends Configured implements Tool {
             }
         }
     }
-    @Override
-    public int run(String[] args) throws Exception
+    public static class AnagramDriver extends Configured implements Tool
     {
-
-        if (args.length != 2)
+        @Override
+        public int run(String[] args) throws Exception
         {
-            System.out.printf("Usage: %s [generic options] <input dir> <output dir>\n", getClass().getSimpleName());
-            ToolRunner.printGenericCommandUsage(System.out);
-            return -1;
+
+            if (args.length != 2)
+            {
+                System.out.printf("Usage: %s [generic options] <input dir> <output dir>\n", getClass().getSimpleName());
+                ToolRunner.printGenericCommandUsage(System.out);
+                return -1;
+            }
+
+            inputPath=args[0];
+            outputPath=args[1];
+
+            cleanInput();
+            JobConf conf = new JobConf(getConf(), AnagramDriver.class);
+            conf.setJobName(this.getClass().getName());
+            FileInputFormat.setInputPaths(conf, new Path(args[0]));
+            FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+            conf.setMapperClass(AnagramMapper.class);
+            conf.setOutputValueGroupingComparator(AnagramOutputValueGroupingComparator.class);
+            conf.setReducerClass(AnagramReducer.class);
+
+            conf.setMapOutputKeyClass(Text.class);
+            conf.setMapOutputValueClass(Text.class);
+
+            conf.setOutputKeyClass(Text.class);
+            conf.setOutputValueClass(Text.class);
+
+            JobClient.runJob(conf);
+            return 0;
         }
 
-        inputPath=args[0];
-        outputPath=args[1];
+        static void cleanInput() throws Exception{
 
-        cleanInput();
-        JobConf conf = new JobConf(getConf(), AnagramSortedValuesDriver.class);
-        conf.setJobName(this.getClass().getName());
-        FileInputFormat.setInputPaths(conf, new Path(args[0]));
-        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+            if(new File(outputPath).isDirectory()){
+                deleteFolder(new File(outputPath));
+            }
+            File dir = new File(inputPath);
+            File[] directoryListing = dir.listFiles();
+            System.out.print(directoryListing[0] );
+            if (directoryListing != null) {
+                for (File child : directoryListing) {
+                    String contents = null;
+                    try {
+                        contents = new String(Files.readAllBytes(Paths.get(child.getPath()))).replaceAll("\\p{Punct}", "");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    String[] lines = contents.split("\\s+");
+                    CloseableHttpClient httpclient = HttpClients.createDefault();
+                    HttpGet httpget = new HttpGet("https://www.textfixer.com/tutorials/common-english-words-with-contractions.txt");
+                    HttpResponse httpresponse = httpclient.execute(httpget);
+                    Scanner sc = new Scanner(httpresponse.getEntity().getContent());
+                    StringBuffer sb = new StringBuffer();
+                    while(sc.hasNext()) {
+                        sb.append(sc.next());
+                    }
+                    String result = sb.toString();
+                    result = result.replaceAll("<[^>]*>", "").toLowerCase();
 
-        conf.setMapperClass(AnagramSortedValuesMapper.class);
-        conf.setOutputValueGroupingComparator(AnagramSortedValuesOutputValueGroupingComparator.class);
-        conf.setReducerClass(AnagramSortedValuesReducer.class);
+                    try {
+                        File f1 = new File(child.getPath());
 
-        conf.setMapOutputKeyClass(Text.class);
-        conf.setMapOutputValueClass(Text.class);
+                        FileWriter fw = new FileWriter(f1);
+                        BufferedWriter out = new BufferedWriter(fw);
+                        for(String s : lines)
+                            if(result.indexOf(s.toLowerCase())==-1) {
+                                out.write(s + "\n");
+                            }
+                        out.flush();
+                        out.close();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        }
 
-        conf.setOutputKeyClass(Text.class);
-        conf.setOutputValueClass(Text.class);
-
-        JobClient.runJob(conf);
-        return 0;
+        static void deleteFolder(File file){
+            for (File subFile : file.listFiles()) {
+                if(subFile.isDirectory()) {
+                    deleteFolder(subFile);
+                } else {
+                    subFile.delete();
+                }
+            }
+            file.delete();
+        }
     }
+
 
     public static void main(String[] args) throws Exception
     {
-        int exitCode = ToolRunner.run(new AnagramSortedValuesDriver(), args);
+        int exitCode = ToolRunner.run(new AnagramDriver(), args);
         System.exit(exitCode);
     }
 
-    static void cleanInput() throws Exception{
-
-        if(new File(outputPath).isDirectory()){
-            deleteFolder(new File(outputPath));
-        }
-        File dir = new File(inputPath);
-        File[] directoryListing = dir.listFiles();
-        System.out.print(directoryListing[0] );
-        if (directoryListing != null) {
-            for (File child : directoryListing) {
-                String contents = null;
-                try {
-                    contents = new String(Files.readAllBytes(Paths.get(child.getPath()))).replaceAll("\\p{Punct}", "");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                String[] lines = contents.split("\\s+");
-                CloseableHttpClient httpclient = HttpClients.createDefault();
-                HttpGet httpget = new HttpGet("https://www.textfixer.com/tutorials/common-english-words-with-contractions.txt");
-                HttpResponse httpresponse = httpclient.execute(httpget);
-                Scanner sc = new Scanner(httpresponse.getEntity().getContent());
-                StringBuffer sb = new StringBuffer();
-                while(sc.hasNext()) {
-                    sb.append(sc.next());
-                }
-                String result = sb.toString();
-                result = result.replaceAll("<[^>]*>", "").toLowerCase();
-
-                try {
-                    File f1 = new File(child.getPath());
-
-                    FileWriter fw = new FileWriter(f1);
-                    BufferedWriter out = new BufferedWriter(fw);
-                    for(String s : lines)
-                        if(result.indexOf(s.toLowerCase())==-1) {
-                            out.write(s + "\n");
-                        }
-                    out.flush();
-                    out.close();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }
-    }
-
-    static void deleteFolder(File file){
-        for (File subFile : file.listFiles()) {
-            if(subFile.isDirectory()) {
-                deleteFolder(subFile);
-            } else {
-                subFile.delete();
-            }
-        }
-        file.delete();
-    }
-
-    
 }
